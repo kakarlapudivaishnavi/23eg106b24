de7ef0ee28c9b353d36264293e53d92bcfd255e9
Kakarlapudivaishnavi <23eg106b24@anurag.edu.in>
2025-10-30 17:37:47 +0530

    Fixed ratings and sidebar text in locations-list

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,38 +3,35 @@ const homelist = (req, res) => {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
       title: 'Loc8r',
-      strapline: 'Find places to work with wifi near you!',
+      strapline: 'Find places to work with wifi near you!'
     },
-    sidebar:
-      'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you’re looking for.',
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
     locations: [
       {
         name: 'Starcups',
         address: '125 High Street, Reading, RG6 1PS',
         rating: 3,
         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        distance: '100m',
+        distance: '100m'
       },
       {
         name: 'Cafe Hero',
         address: '125 High Street, Reading, RG6 1PS',
         rating: 4,
         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        distance: '200m',
+        distance: '200m'
       },
       {
         name: 'Burger Queen',
         address: '125 High Street, Reading, RG6 1PS',
         rating: 2,
         facilities: ['Food', 'Premium wifi'],
-        distance: '500m',
-      },
-    ],
+        distance: '500m'
+      }
+    ]
   });
 };
 
-module.exports = {
-  homelist,
-};
+module.exports = { homelist };
 
 
