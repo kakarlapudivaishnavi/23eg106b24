89e19040b1cc177b89a2ef83201f97e0f2d7b1e1
Kakarlapudivaishnavi <23eg106b24@anurag.edu.in>
2025-10-30 17:08:15 +0530

    Added missing locations to controller

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 
-/* GET home page */
 router.get('/', ctrlLocations.homelist);
 
 module.exports = router;
